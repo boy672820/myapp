1e46401764de0ebaacef2890b9d0e830deebabcb
boy672820 <boy672820@gmail.com>
2016-10-08 23:05:22 +0900

    MongoDB 사용

--- app.js
+++ app.js
@@ -1,11 +1,63 @@
+// Modules
 var express = require( 'express' );
-
+var mongoose = require( 'mongoose' );
+var path = require( 'path' );
+// Express
 var app = express();
 
-app.get( "/", function ( req, res ) {
-  res.send( "Hello world..!" );
+
+// Mongodb settings
+mongoose.connect( 'mongodb://mst:1234@ds053156.mlab.com:53156/myapp_db' );
+var db = mongoose.connection;
+
+db.once( 'open', function () {
+  console.log( 'DB Connected!' );
+} );
+db.on( 'error', function ( error ) {
+  console.log( 'DB Error: ', error );
 } );
 
+var DataSchema = mongoose.Schema( {
+  name: String,
+  count: Number
+} );
+
+var Data = mongoose.model( 'data', DataSchema );
+
+
+// App settings
+app.set( 'view engine', 'ejs' );
+//app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+
+// Learning Node.js & Express framework coding!
+app.get( '/', function ( req, res ) {
+  Data.findOne( { name: 'myData' }, function ( error, data ) {
+    if ( error ) return console.log( 'Data Error: ', error );
+
+    data.count += 1;
+    data.save( function ( error ) {
+      if ( error ) return console.log( 'Data Error: ', error );
+
+      res.render( 'index', data );
+    } );
+  } );
+} );
+/*
+app.get( '/reset', function( req, res ) {
+  data.count = 0;
+  res.render( 'index', data );
+} );
+app.get( '/set/count', function ( req, res ) {
+  if ( req.query.count ) data.count = parseInt( req.query.count );
+  res.render( 'index', data );
+} );
+app.get( '/set/:num', function ( req, res ) {
+  data.count = parseInt( req.params.num );
+  res.render( 'index', data );
+} );
+*/
+// Listen HTTP Server
 app.listen( 8000, function () {
-  console.log( "Server on!" );
+  console.log( 'Server on!' );
 } );
