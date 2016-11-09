e4072fdf874a954976df0300902bf4c8690bf4a2
boy672820 <boy672820@gmail.com>
2016-11-09 11:09:26 +0900

    RESTful CRUD 구현

--- README.md
+++ README.md
@@ -7,6 +7,11 @@
 + Angular.js - 프론트엔드 MVC 개발 프레임워크
 + MongoDB - NoSQL 기반 데이터베이스
 
+### 2016-11-09
++ RESTful CRUD 구현
++ body-parser package 추가 & body-parser를 미들웨어로 사용하는데, 요청 데이터들의 body를 JSON으로 분석한다.
++ Insomnia(REST Clinet)를 이용한 REST Requests 테스트
+
 ### 2016-10-08
 + MongoDB Online - myapp_db
 + Mongoose 모듈 추가
--- app.js
+++ app.js
@@ -1,63 +1,90 @@
-// Modules
+// Import modules
 var express = require( 'express' );
 var mongoose = require( 'mongoose' );
 var path = require( 'path' );
-// Express
-var app = express();
+var bodyParser = require( 'body-parser' );
 
 
-// Mongodb settings
-mongoose.connect( 'mongodb://mst:1234@ds053156.mlab.com:53156/myapp_db' );
+// Connect database
+mongoose.connect( process.env.MONGODB );
 var db = mongoose.connection;
-
 db.once( 'open', function () {
-  console.log( 'DB Connected!' );
+	console.log( 'DB Connected!' );
 } );
 db.on( 'error', function ( error ) {
-  console.log( 'DB Error: ', error );
+	console.log( 'DB Error: ', error );
 } );
 
-var DataSchema = mongoose.Schema( {
-  name: String,
-  count: Number
+var postSchema = mongoose.Schema( {
+	title: { type: String, required: true },
+	body: { type: String, required: true },
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: Date
 } );
+var Post = mongoose.model( 'post', postSchema );
 
-var Data = mongoose.model( 'data', DataSchema );
 
+// Express app instance
+var app = express();
 
-// App settings
+// View setting
+// 뷰 엔진 :: Embedded JavaScript
 app.set( 'view engine', 'ejs' );
-//app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+// Set use middlewares
+/** 미들웨어(Middlewares) 란?
+ * 사용자 요청(Request)에 대한 서버로 부터의 응답(Response)은 라우터(Router)를 통해 어떤 응답을 할지 정한다.
+ * 미들웨어는 라우터를 통하기 전의 모든 신호들에게 수행되는 명령어이다.
+ * app.use() 를 통해 수행될 수 있으며, 해당 명령어는 라우터보다 위에 위치해야 한다.
+ */
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use( bodyParser.json() );
+
+var isError = function ( error, res ) {
+	if ( error )
+		return res.json( { success: false, message: error } );
+};
+
+// POST:: Create
+app.post( '/posts', function ( req, res ) {
+	Post.create( req.body.post, function ( error, post ) {
+		isError( error, res );
+		res.json( { success: true, data: post } );
+	} );
+} );
+// GET:: Index
+app.get( '/posts', function ( req, res ) {
+	Post.find( {}, function ( error, posts ) {
+		isError( error, res );
+		res.json( { success: true, data: posts } );
+	} );
+} );
+// GET:: Show
+app.get( '/posts/:id', function ( req, res ) {
+	Post.findById( req.params.id, function ( error, post ) {
+		isError( error, res );
+		res.json( { success: true, data: post } );
+	} );
+} );
+// PUT:: Update
+app.put( '/posts/:id', function( req, res ) {
+	req.body.post.updatedAt = Date.now();
+
+	Post.findByIdAndUpdate( req.params.id, req.body.post, function ( error, post ) {
+		isError( error, res );
+		res.json( { success: true, message: post._id + ' updated', data: post } );
+	} );
+} );
+// DELETE:: Destroy
+app.delete( '/posts/:id', function ( req, res ) {
+	Post.findByIdAndRemove( req.params.id, function ( error, post ) {
+		isError( error, res );
+		res.json( { success: true, message: post._id + ' deleted' } );
+	} );
+} );
 
 
-// Learning Node.js & Express framework coding!
-app.get( '/', function ( req, res ) {
-  Data.findOne( { name: 'myData' }, function ( error, data ) {
-    if ( error ) return console.log( 'Data Error: ', error );
-
-    data.count += 1;
-    data.save( function ( error ) {
-      if ( error ) return console.log( 'Data Error: ', error );
-
-      res.render( 'index', data );
-    } );
-  } );
-} );
-/*
-app.get( '/reset', function( req, res ) {
-  data.count = 0;
-  res.render( 'index', data );
-} );
-app.get( '/set/count', function ( req, res ) {
-  if ( req.query.count ) data.count = parseInt( req.query.count );
-  res.render( 'index', data );
-} );
-app.get( '/set/:num', function ( req, res ) {
-  data.count = parseInt( req.params.num );
-  res.render( 'index', data );
-} );
-*/
 // Listen HTTP Server
 app.listen( 8000, function () {
-  console.log( 'Server on!' );
+	console.log( 'Server on!' );
 } );
