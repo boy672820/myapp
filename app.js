02d4df297add483fd102c8985f869b6734f7c493
boy672820 <boy672820@gmail.com>
2016-11-18 17:48:20 +0900

    RESTful CRUD 구현

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require( 'express' );
 var mongoose = require( 'mongoose' );
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
+var methodOverride = require( 'method-override' );
 
 
 // Connect database
+mongoose.Promise = global.Promise;
 mongoose.connect( process.env.MONGODB );
 var db = mongoose.connection;
 db.once( 'open', function () {
@@ -39,47 +41,73 @@ app.set( 'view engine', 'ejs' );
  */
 app.use( express.static( path.join( __dirname, 'public' ) ) );
 app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( methodOverride( '_method' ) );
 
+// isError()
 var isError = function ( error, res ) {
 	if ( error )
 		return res.json( { success: false, message: error } );
 };
 
+// Set routes
+
+// GET:: Index
+app.get( '/posts', function ( req, res ) {
+	/**
+	 * find에서 바로 콜백 함수가 호출되지 않고, find으로 데이터를 찾고, sort로 자료를 정렬하고, exec로 함수를 수정하는 형태이다.
+	 * sort로 자료 정렬 시 '-createdAt'의 '-'는 역방향으로 정렬을 하라는 뜻이다. 그냥 'createdAt'으로 하면 정방향으로 정렬된다.
+	 */
+	Post.find( {} ).sort( '-createdAt' ).exec( function ( error, posts ) {
+		isError( error, res );
+		res.render( 'posts/index', { data: posts } );
+	} );
+} );
+
+// GET:: New
+app.get( '/posts/new', function ( req, res ) {
+	res.render( 'posts/new' );
+} );
+
 // POST:: Create
 app.post( '/posts', function ( req, res ) {
 	Post.create( req.body.post, function ( error, post ) {
 		isError( error, res );
-		res.json( { success: true, data: post } );
+		res.redirect( '/posts' );
 	} );
 } );
-// GET:: Index
-app.get( '/posts', function ( req, res ) {
-	Post.find( {}, function ( error, posts ) {
-		isError( error, res );
-		res.json( { success: true, data: posts } );
-	} );
-} );
+
 // GET:: Show
 app.get( '/posts/:id', function ( req, res ) {
 	Post.findById( req.params.id, function ( error, post ) {
 		isError( error, res );
-		res.json( { success: true, data: post } );
+		res.render( 'posts/show', { data: post } );
 	} );
 } );
+
+// GET:: Edit
+app.get( '/posts/edit/:id', function ( req, res ) {
+	Post.findById( req.params.id, function ( error, post ) {
+		isError( error, res );
+		res.render( 'posts/edit', { data: post } );
+	} );
+} );
+
 // PUT:: Update
 app.put( '/posts/:id', function( req, res ) {
 	req.body.post.updatedAt = Date.now();
 
 	Post.findByIdAndUpdate( req.params.id, req.body.post, function ( error, post ) {
 		isError( error, res );
-		res.json( { success: true, message: post._id + ' updated', data: post } );
+		res.redirect( '/posts/' + req.params.id );
 	} );
 } );
+
 // DELETE:: Destroy
 app.delete( '/posts/:id', function ( req, res ) {
 	Post.findByIdAndRemove( req.params.id, function ( error, post ) {
 		isError( error, res );
-		res.json( { success: true, message: post._id + ' deleted' } );
+		res.redirect( '/posts' );
 	} );
 } );
 
